eeaf17365f0316f93f5d01ec2e57ce447d0097b0
Viktor Ádám Trefiman <varazsgoreny765@gmail.com>
2025-04-09 13:40:26 +0200

    Add project files.

---
+++ Covid/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Covid;
+
+internal class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Járvány van. Maradj otthon!");
+
+		Vakcina a = new Astrazeneca(new DateOnly(2025, 10, 1));
+		Vakcina m = new Moderna(new DateOnly(2025, 9, 1));
+		Vakcina p = new Moderna(new DateOnly(2025, 1, 1));
+
+		Paciens peti = new("69420");
+
+		Oltohely heimpal = new("Heimpál gyermekkórház");
+
+		heimpal.Beszerez(a);
+		heimpal.Beszerez(m);
+		heimpal.Beszerez(p);
+
+		heimpal.Regisztral(peti);
+
+		heimpal.Beolt(peti, "pfizer");
+		heimpal.Beolt(peti, "moderna");
+
+		Console.WriteLine(heimpal.HanyanKettot());
+		Console.WriteLine(peti.HanyOltas());
+	}
+}
